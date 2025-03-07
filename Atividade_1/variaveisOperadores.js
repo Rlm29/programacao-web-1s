3cf976a718ed4a8ca07c7586af60c4dd1520b524
Rlm29 <145131286+Rlm29@users.noreply.github.com>
2025-03-06 23:31:28 -0300

    Add files via upload

---
+++ Atividade_1/variaveisOperadores.js
@@ -0,0 +1,15 @@
+let numA = 5.0;
+let numB = 2.5;
+let numC; 
+
+numC = numA + numB;
+console.log(`Resultado da adição entre ${numA} e ${numB} é ${numC}`);
+
+numC = numA - numB;
+console.log(`Resultado da subtração entre ${numA} e ${numB} é ${numC}`);
+
+numC = numA * numB;
+console.log(`Resultado da multiplicação entre ${numA} e ${numB} é ${numC}`);
+
+numC = numA / numB;
+console.log(`Resultado da divisão entre ${numA} e ${numB} é ${numC}`);
